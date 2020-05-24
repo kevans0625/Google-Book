b4f2ad3c56e13a29df930ab1b5bcc0f2a1ab1736
Kristin Evans <kevans0625@gmail.com>
2020-05-24 09:08:36 -0500

    set up SAVED  result components

---
+++ client/src/components/SavedCard.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+function SavedResults(){
+ return (
+    <div className="max-w-sm w-full lg:max-w-full lg:flex">
+    <img className="image-div h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"  src="https://api.adorable.io/avatars/285/abott@adorable.png" alt="Avatar of Jonathan Reinink"/>
+        
+    <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+     
+      <div className="mb-8">
+  
+        <button class="bg-red-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex border border-gray-400 rounded shadow">
+    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+    <path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z" />
+          </svg>
+    <span>Delete</span>
+  </button>
+  <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+     <span>View</span>
+  </button>
+        <div className="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
+        <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+      </div>
+      <div className="flex items-center">
+        <img className="w-10 h-10 rounded-full mr-4" src="https://api.adorable.io/avatars/285/abott@adorable.png" alt="Avatar of Jonathan Reinink"/>
+        <div className="text-sm">
+          <p className="text-gray-900 leading-none">Jonathan Reinink</p>
+        </div>
+      </div>
+    </div>
+  </div>
+ )
+}
+export default SavedResults;
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,8 +1,14 @@
 import React from "react"
+import SavedResults from "../components/SavedCard"
 
 function Saved(){
  return (
-     <h1> Saved</h1>
+     <div>
+     <br/>
+   <div className="text-gray-900 font-bold text-xl mb-2">SAVED BOOKS</div>
+   <br/>
+     <SavedResults/>
+     </div>
  )
 }
 export default Saved;
