b03239dd394c8de77a1c1871ef6d95b66f35a226
Kristin Evans <kevans0625@gmail.com>
2020-05-23 23:08:55 -0500

    set up routers for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
@@ -9,12 +9,14 @@ import Search from "./pages/Search";
 
 function App() {
   return (
+      <Router>
     <div className="App">
       <Header/>
-        <SearchBar/>
-       <Saved />
-        <Search />
+      <SearchBar/>
+      <Route exact path="/" component={Search} />
+      <Route exact path="/about" component={Saved} />
     </div>
+    </Router>
   );
 }
 
