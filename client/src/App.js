1ba422cd8f0b63657d33fec028fdedc077c3b6a8
Kristin Evans <kevans0625@gmail.com>
2020-05-23 21:41:42 -0500

    created and styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+
 import "./App.css";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
+
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <Header/>
+        <SearchBar/>
+       <Saved />
+        <Search />
     </div>
   );
 }
---
+++ client/src/pages/Saved.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+function Saved(){
+ return (
+     <h1> Saved</h1>
+ )
+}
+export default Saved;
