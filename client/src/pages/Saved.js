c9b5ffe90757495d891cadf8ce29c2093d4f0358
Kristin Evans <kevans0625@gmail.com>
2020-05-26 21:28:21 -0500

    updated saved pages

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,14 +1,50 @@
-import React from "react"
-import SavedResults from "../components/SavedCard"
+import React, { useEffect, useState } from "react";
+
+import API from "../utils/API";
+// import SavedResults from "../components/SavedCard"
+import {List, ListItem} from "../components/BookSearchCard";
 
 function Saved(){
+
+  const [books, setBooks] = useState([])
+  // const [formObject, setFormObject] = useState({})
+
+  useEffect(() => {
+    loadBooks()
+  }, [])
+
+  function loadBooks() {
+    API.getBooks()
+      .then(res => 
+       { console.log(res)
+        setBooks(res.data)}
+      )
+      .catch(err => console.log(err));
+  };
+
+  let bookSet = books.results
  return (
      <div>
      <br/>
    <div className="text-gray-900 font-bold text-xl mb-2">SAVED BOOKS</div>
    <br/>
-     <SavedResults/>
+   {bookSet ? (
+                  <List>
+                {bookSet.map(book =>(
+             <ListItem key={book.id}>
+               {/* <Link to={"/books/" + book._id}> */}
+                 <strong>
+                   {book.title} by {book.author}
+                 </strong>
+               {/* </Link> */}
+               {/* <DeleteBtn onClick={() => deleteBook(book._id)} /> */}
+             </ListItem>
+           ))}
+         </List>
+           ) : (
+            <h3>No Results to Display</h3>
+          )}
      </div>
- )
+ );
 }
 export default Saved;
