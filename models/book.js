644dd1b4f11d56a689511aebc54c66bbea466fde
Kristin Evans <kevans0625@gmail.com>
2020-05-28 14:49:40 -0500

    fixed typo in image

--- models/book.js
+++ models/book.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
 const bookSchema = new Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    description: { type: String, required: true },
-    iamge: { type: String, required: true },
-    link: { type: String, required: true },
+    title: { type: String},
+    author: { type: String},
+    description: { type: String},
+    image: { type: String},
+    link: { type: String},
     date: { type: Date, default: Date.now }
 });
 
